eab9316654ef7629ef04ec8e6ed7819c6ab3a660
DESKTOP-H8JOB5A\Mashuri_Mansur <mashurimansur18@gmail.com>
2017-08-23 18:40:19 +0800

    LOGIN

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,89 +4,73 @@
 
 <head>
 
-    <meta name="keyword" content="">
-
-    <meta name="description" content="">
-
-    <meta name="author" content="">
-
-    <meta property="og:image" content="{{ url('/resources/assets/app') }}/img/logo.svg" />
-
-    <meta charset="UTF-8">
-
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @include('app.layouts.partial.meta')
 
     <title>Kotakarsip</title>
 
-    <link rel="icon" sizes="16x16" href="{{ url('/resources/assets/app') }}/img/favicon.png" />
-
-    <!-- Font Icon -->
-    <link rel="stylesheet" href="{{ url('/resources/assets/app') }}/libs/font-icons/entypo/css/entypo.css">
-    <link rel="stylesheet" href="{{ url('/resources/assets/app') }}/libs/font-icons/font-awesome/css/font-awesome.min.css">
-    
-    <!-- Libs css -->
-    <link rel="stylesheet" href="{{ url('/resources/assets/app') }}/libs/jquery-ui/jquery-ui.css">
-    <link href="{{ url('/resources/assets/app') }}/libs/sumoselect/sumoselect.css" rel="stylesheet" />
-    <link rel="stylesheet" type="text/css" href="{{ url('/resources/assets/app') }}/libs/datepick/jquery.datepick.css">
-
-    <!-- Custom css -->
-    <link href="{{ url('/resources/assets/app') }}/css/kotakarsip.css" rel="stylesheet">
+    @include('app.layouts.partial.style')
 
 </head>
 
 
-<body>
+<body class="page-login">
 
-    <section class="login">
-        <form action="{{ route('login') }}" class="form-custom row" method="POST">
-            {{ csrf_field() }}
-            <div class="text-center brand">
-                <br>
-                <img src="{{ url('/resources/assets/app') }}/img/logo.svg" alt="Logo Kotakarsip">
-                <br><br><br>
-            </div>
-            <div class="col-md-12">
-                @if ( $errors->has('email') or $errors->has('password') )
-                    <div class="alert-top alert alert-danger text-center">Email atau kata sandi salah</div>
-                @endif
-                <div class="form-group">
-                    <label class="form-label" for="">Nama Pengguna / Email</label>
-                    <input type="text" name="email" value="{{ old('email') }}" class="form-control" data-validation="required" data-validation-error-msg-required="* Wajib diisi">
-                </div>
-                <div class="form-group">
-                    <label class="form-label" for="">Kata Sandi</label>
-                    <input type="password" name="password" class="form-control" data-validation="required" data-validation-error-msg-required="* Wajib diisi">
-                </div>
-                
-                <small class="color-gray" style="margin-bottom: 10px; display: block;"><i>Jangan dicentang jika ini komputer publik</i></small>
+    <div id="app">
+        <nav class="ka-nav ka-nav-detail">
+            <ul class="left-side">
+                <li class="brand">
+                    <img src="{{ asset('assets/app/img/logo.svg') }}" class="logo" alt="Logo KotakArsip"> &nbsp;&nbsp;<b>KOTAK<span>ARSIP</span></b>
+                </li>
+            </ul>
+            <ul class="right-side">
+                <li><a href="">Laporkan masalah</a></li>
+                <li>
+                    <a href="{{ route('register') }}" class="btn btn-primary">Daftar</a>
+                </li>
+            </ul>
+        </nav>
 
+        <div class="ka-body">
+            <div class="container">
                 <div class="row">
-                    <div class="form-group col-md-6">
-                        <div class="checkbox-single">
-                            <input type="checkbox" id="check-1" name="remember" {{ old('remember') ? 'checked' : '' }}>
-                            <label for="check-1"><i class="fa fa-check"></i></label>
-                            <label class="label-custom" for="check-1">Tetap masuk</label>
-                        </div>
+                    <div class="col-md-offset-2 col-md-4">
+                        <img src="{{ asset('assets/app/img/login.svg') }}" alt="" width="98%" style="margin-top: 60px">
                     </div>
-                    <div class="form-group col-md-6 text-right">
-                        <a href="{{ route('password.request') }}">Forgot Password</a>
+                    <div class="col-md-4">
+                        <h1>Masuk</h1>
+
+                        <form action="{{ route('login') }}" class="form-custom row" method="POST">
+                            {{ csrf_field() }}
+                            @if ( $errors->has('email') or $errors->has('password') )
+                                <div class="alert-top alert alert-danger text-center">Email atau kata sandi salah</div>
+                            @endif
+                            <div class="form-group">
+                                <input type="text" name="email" value="{{ old('email') }}" class="form-control" placeholder="Alamat Email">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" name="password" class="form-control" placeholder="Kata Sandi">
+                            </div>
+                            <div class="form-group row">
+                                <div class="col-md-6">
+                                    <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> &nbsp;Tetap masuk
+                                </div>
+                                <div class="col-md-6">
+                                    <button class="btn btn-primary btn-block">Masuk</button>
+                                </div>
+                            </div>
+                        </form>
+
+                        <hr>
+
+                        <a href="{{ route('password.request') }}">Lupa kata sandi?</a>
                     </div>
                 </div>
-                
-                <button class="btn btn-info btn-block" lang-id="Masuk">Login</button>
-
-                <br>
             </div>
-        </form>
-    </section>
+        </div>
+    </div>
 
-    <ul class='right-click right-click-doc'>
-        <li><a href="index.html">Kembali</a></li>
-    </ul>
-
-    <script src="{{ url('/resources/assets/app') }}/js/kotakarsip.js"></script>
+     @include('app.layouts.partial.script')
 
 </body>
 
 </html>
-                    
