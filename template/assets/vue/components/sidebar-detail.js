63f3d6757157d4302d79268619367b8ecd1754ff
Gifa Eriyanto <gifaeriyanto@ymail.com>
2017-08-30 09:05:19 +0800

    v-html filter

--- template/assets/vue/components/sidebar-detail.js
+++ template/assets/vue/components/sidebar-detail.js
@@ -2,6 +2,14 @@ Vue.component('detail', {
 	template: '#sidebar-detail',
 	props: {
 		detail: Object
+	},
+	filters: {
+		moment: function (date) {
+			var day = moment(date, "x").date();
+			var month = moment(date, "x").month();
+			var year = moment(date, "x").year();
+			return day + "/" + month + "/" + year;
+		}
 	}
 });
 
