0e2f520e896126b937920bef675aa9e777ecacdf
Mashuri_Mansur <mashurimansur18@gmail.com>
2017-09-19 13:33:18 +0800

    CSRF Token

--- resources/views/app/layouts/partial/meta.blade.php
+++ resources/views/app/layouts/partial/meta.blade.php
@@ -9,3 +9,5 @@
 <meta charset="UTF-8">
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+
+<meta name="csrf_token" content="{{ csrf_token() }}">
--- resources/views/app/layouts/partial/script.blade.php
+++ resources/views/app/layouts/partial/script.blade.php
@@ -1,2 +1,16 @@
 <script src="{{ asset('assets/app/js/kotakarsip.js') }}"></script>
 <script src="{{ asset('assets/app/vue/components/sidebar-detail.js') }}"></script>
+
+<script type="text/javascript">
+	// refreshToken
+	var csrfToken = $('[name="csrf_token"]').attr('content');
+
+	setInterval(refreshToken, 3600000); // 1 hour 
+
+	function refreshToken(){
+		$.get('refresh-csrf').done(function(data){
+			csrfToken = data; // the new token
+		});
+	}
+	setInterval(refreshToken, 3600000); // 1 hour 
+</script>
