3855a4029b6fbe44bd499dfc4c770a91fa740b9f
Mashuri_Mansur <mashurimansur18@gmail.com>
2017-09-24 22:51:23 +0800

    api pencarian

--- app/Http/Controllers/App/SearchController.php
+++ app/Http/Controllers/App/SearchController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\App;
-
+use App\Archieve;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -17,8 +17,126 @@ class SearchController extends Controller
 		return view('app.search.index');
 	}
 
-	public function getData()
+	public function getData(Request $r)
 	{
 
+		$archieve = Archieve::raw(function($collection){
+
+			$tes = @$_GET['q'];
+
+			return $collection->aggregate(array(
+				array(
+					'$unwind' => array(
+						'path' => '$share',
+						'preserveNullAndEmptyArrays' => true
+					)
+				),
+				array(
+					'$lookup' => array(
+						'from'=>'users',
+						'localField'=>'share._id',
+						'foreignField'=>'_id',
+						'as'=>'share.user'
+					)
+				),
+				array(
+					'$lookup' => array(
+						'from' => 'storage_sub',
+						'localField' => 'storagesub',
+						'foreignField' =>  '_id',
+						'as' => 'storagesub'
+					)
+				),
+				array(
+					'$lookup' => array(
+						'from' => 'storage',
+						'localField' => 'storage',
+						'foreignField' =>  '_id',
+						'as' => 'storage'
+					)
+				),
+				array(
+					'$project' => array(
+						'from' => 1,
+						'reference_number' => 1,
+						'date' => 1,
+						'subject' => 1,
+						'share.user._id' => 1,
+						'share.user.name' => 1,
+						'share.user.position' => 1,
+						'share.user.photo' => 1,
+						'share.date' => 1,
+						'share.message' => 1,
+						'storagesub._id' => 1,
+						'storagesub.name' => 1,
+						'storage._id' => 1,
+						'storage.name' => 1,
+						'files' => 1,
+						'type' => 1,
+						'id_user' => 1,
+						'id_company' => 1,
+						'deleted_at' => 1
+					)
+				),
+				array(
+					'$group' => array(
+						'_id' => '$_id',
+						'id_user' => array(
+							'$first' => '$id_user'
+						),
+						'id_company' => array(
+							'$first' => '$id_company'
+						),
+						'type' => array(
+							'$first' => '$type'
+						),
+						'from' => array(
+							'$first' => '$from'
+						),
+						'subject' => array(
+							'$first' => '$subject'
+						),
+						'reference_number' => array(
+							'$first' => '$reference_number'
+						),
+						'date' => array(
+							'$first' => '$date'
+						),
+						'reference_number' => array(
+							'$first' => '$reference_number'
+						),
+						'storagesub' => array(
+							'$first' => '$storagesub'
+						),
+						'storage' => array(
+							'$first' => '$storage'
+						),
+						'files' => array(
+							'$first' => '$files'
+						),
+						'deleted_at' => array(
+							'$first' => '$deleted_at'
+						),
+						'share' => array(
+							'$push' => array(
+								'user' => '$share.user',
+								'date' => '$share.date',
+								'message' => '$share.message'
+							)
+						)
+					)
+				),
+				array(
+					'$match' => array(
+						'from' => array(
+							'$regex' => $tes,
+							'$options' => 'i'
+						)
+					)
+				)
+			));
+		});
+		
+		return response()->json($archieve);
 	}
 }
