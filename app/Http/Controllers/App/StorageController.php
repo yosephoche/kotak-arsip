993d312a5e194ae26b014ad734efc024decdad51
Gifa Eriyanto <gifaeriyanto@ymail.com>
2017-11-22 02:13:55 +0800

    remove created_at dan updated_at

--- app/Http/Controllers/App/StorageController.php
+++ app/Http/Controllers/App/StorageController.php
@@ -8,143 +8,135 @@ use Auth;
 
 class StorageController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-    
-    public function index()
-    {
-        return view('app.storage.index');
-    }
+	public function __construct()
+	{
+		$this->middleware('auth');
+	}
+	
+	public function index()
+	{
+		return view('app.storage.index');
+	}
 
-    public function getData()
-    {
-        $storage = Storage::raw(function($collection){
+	public function getData()
+	{
+		$storage = Storage::raw(function($collection){
 
-            return $collection->aggregate(array(
-                array(
-                    '$lookup' => array(
-                        'from'=>'archieve',
-                        'localField'=>'_id',
-                        'foreignField'=>'storage',
-                        'as'=>'count'
-                    )
-                ),
-                array(
-                    '$unwind' => array(
-                        'path' => '$count',
-                        'preserveNullAndEmptyArrays' => true
-                    )
-                ),
-                array(
-                    '$match' => array(
-                        'id_company' => Auth::user()->id_company,
-                        'count.deleted_at' => null
-                    )
-                ),
-                array(
-                    '$group' => array(
-                        '_id' => '$_id',
-                        'name' => array(
-                            '$first' => '$name'
-                        ),
-                        'type' => array(
-                            '$first' => '$type'
-                        ),
-                        'created_at' => array(
-                            '$first' => '$created_at'
-                        ),
-                        'updated_at' => array(
-                            '$first' => '$updated_at'
-                        ),
-                        'count' => array(
-                            '$push' => array(
-                                '_id' => '$count._id',
-                            )
-                        )
-                    )
-                ),
-               
-                array(
-                    '$project' => array(
-                        '_id' => 1,
-                        'name' => 1,
-                        'type' => 1,
-                        'count' => array(
-                            '$size' => '$count._id'
-                        ),
-                        'created_at' => 1,
-                        'updated_at' => 1
-                    )
-                )
-            ));
-        });
+			return $collection->aggregate(array(
+				array(
+					'$lookup' => array(
+						'from'=>'archieve',
+						'localField'=>'_id',
+						'foreignField'=>'storage',
+						'as'=>'count'
+					)
+				),
+				array(
+					'$unwind' => array(
+						'path' => '$count',
+						'preserveNullAndEmptyArrays' => true
+					)
+				),
+				array(
+					'$match' => array(
+						'id_company' => Auth::user()->id_company,
+						'count.deleted_at' => null
+					)
+				),
+				array(
+					'$group' => array(
+						'_id' => '$_id',
+						'name' => array(
+							'$first' => '$name'
+						),
+						'type' => array(
+							'$first' => '$type'
+						),
+						'count' => array(
+							'$push' => array(
+								'_id' => '$count._id',
+							)
+						)
+					)
+				),
+			   
+				array(
+					'$project' => array(
+						'_id' => 1,
+						'name' => 1,
+						'type' => 1,
+						'count' => array(
+							'$size' => '$count._id'
+						),
+					)
+				)
+			));
+		});
 
-        return response()->json(['storage' => $storage]);
-    }
+		return response()->json(['storage' => $storage]);
+	}
 
-    public function register()
-    {
-        return view('app.storage.register');
-    }
+	public function register()
+	{
+		return view('app.storage.register');
+	}
 
-    public function success()
-    {
-        return view('app.storage.success');
-    }
+	public function success()
+	{
+		return view('app.storage.success');
+	}
 
-    public function store(Request $r)
-    {
-        $this->validate($r, [
-            'name' => 'required',
-            'type' => 'required'
-        ]);
+	public function store(Request $r)
+	{
+		$this->validate($r, [
+			'name' => 'required',
+			'type' => 'required'
+		]);
 
-        //Storage Store
-        $storage = new Storage;
-        $storage->name = $r->name;
-        $storage->type = $r->type;
-        $storage->id_company = Auth::user()->id_company;
-        $storage->save();
+		//Storage Store
+		$storage = new Storage;
+		$storage->name = $r->name;
+		$storage->type = $r->type;
+		$storage->id_company = Auth::user()->id_company;
+		$storage->save();
 
-        $r->session()->flash('success', 'Penyimpanan arsip baru berhasil didisposisi');
+		$r->session()->flash('success', 'Penyimpanan arsip baru berhasil didisposisi');
 
-        return redirect()->back();
-    }
+		return redirect()->back();
+	}
 
-    public function edit($id)
-    {
-        $data['storage'] = Storage::find($id);
+	public function edit($id)
+	{
+		$data['storage'] = Storage::find($id);
 
-        return view('app.storage.edit', $data);
-    }
+		return view('app.storage.edit', $data);
+	}
 
-    public function update(Request $r)
-    {
-        $this->validate($r, [
-            'name' => 'required',
-            'type' => 'required'
-        ]);
+	public function update(Request $r)
+	{
+		$this->validate($r, [
+			'name' => 'required',
+			'type' => 'required'
+		]);
 
-        //Storage Update
-        $storage = Storage::find($r->id);
-        $storage->name = $r->name;
-        $storage->type = $r->type;
-        $storage->id_company = Auth::user()->id_company;
-        $storage->save();
+		//Storage Update
+		$storage = Storage::find($r->id);
+		$storage->name = $r->name;
+		$storage->type = $r->type;
+		$storage->id_company = Auth::user()->id_company;
+		$storage->save();
 
-        $r->session()->flash('success', 'Berhasil menyimpan pembaruan');
+		$r->session()->flash('success', 'Berhasil menyimpan pembaruan');
 
-        return redirect()->back();
-    }
+		return redirect()->back();
+	}
 
-    public function delete(Request $r)
-    {
-        Storage::where('_id', $r->id)->delete();
+	public function delete(Request $r)
+	{
+		Storage::where('_id', $r->id)->delete();
 
-        $r->session()->flash('success', 'Penyimpanan arsip berhasil dihapus');
+		$r->session()->flash('success', 'Penyimpanan arsip berhasil dihapus');
 
-        return redirect()->back();
-    }
+		return redirect()->back();
+	}
 }
